82bca2281fb75b73944dbfd5a78507345f5ffa7a
Ashley <ashleylurombo@gmail.com>
2022-12-23 17:37:47 +0200

    removed unused resoruces fixed dependecy issues

--- app/src/main/java/mad/app/madandroidtestsolutions/MobileShoppingBaseFragment.kt
+++ app/src/main/java/mad/app/madandroidtestsolutions/MobileShoppingBaseFragment.kt
@@ -1,7 +1,7 @@
 package mad.app.madandroidtestsolutions
 //This class is not necessarily needed seeing as this is a small project but will be helpful in bigger projects
 
-import android.support.annotation.LayoutRes
+import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 
